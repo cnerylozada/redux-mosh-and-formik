e180e792e85fd485f562cd5b2f1ef871dbede708
cnerylozada <cnery@dynamicdevs.io>
2021-05-12 21:38:12 -0500

    feat: creating a reducer

---
+++ src/redux/reducer.js
@@ -0,0 +1,23 @@
+const bugAdded = "BUG_ADDED";
+const bugRemoved = "BUG_REMOVED";
+
+const initialState = [];
+let idCounter = 0;
+
+export const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case bugAdded:
+      return [
+        ...state,
+        {
+          id: ++idCounter,
+          description: action.payload.description,
+          resolved: false,
+        },
+      ];
+    case bugRemoved:
+      return state.filter((_) => _.id !== action.payload.id);
+    default:
+      return state;
+  }
+};
