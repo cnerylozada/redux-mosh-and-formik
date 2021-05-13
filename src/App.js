d02380e99caf016a9a88028854f344dd069a010e
cnerylozada <cnery@dynamicdevs.io>
2021-05-12 22:03:24 -0500

    feat: subscribing to the store

--- src/App.js
+++ src/App.js
@@ -4,14 +4,14 @@ import { store } from "./redux/store";
 
 const App = () => {
   useEffect(() => {
-    console.log(store.getState());
+    store.subscribe(() => {
+      console.log(store.getState());
+    });
 
     store.dispatch(addBug("mi first bug!"));
     store.dispatch(addBug("mi second bug!"));
-    console.log(store.getState());
 
     store.dispatch(removeBugById(1));
-    console.log(store.getState());
   });
   return <div className="App"></div>;
 };
