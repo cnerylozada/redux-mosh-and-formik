5c5a5aed19b22d9a959cf1b8ee2b49f3db6ec855
cnerylozada <cnery@dynamicdevs.io>
2021-05-21 11:28:37 -0500

    feat: menu-accordion was implemented

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 
 const LayoutPage = () => {
-  return <div></div>;
+  const contentList = [
+    {
+      title: "section 1",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      title: "section 2",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+    {
+      title: "section 3",
+      content:
+        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    },
+  ];
+  return (
+    <div>
+      <menu-accordion
+        content-list={JSON.stringify(contentList)}
+      ></menu-accordion>
+    </div>
+  );
 };
 
 export default LayoutPage;
