97844ea22d26ecdb72f50cebf842c070a9630710
cnerylozada <cnery@dynamicdevs.io>
2021-05-27 23:02:53 -0500

    feat: stencil components

--- src/pages/layout.jsx
+++ src/pages/layout.jsx
@@ -1,28 +1,29 @@
 import React from "react";
 
 const LayoutPage = () => {
-  const contentList = [
+  const arrayData = [
     {
       title: "section 1",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Content 1 dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
       title: "section 2",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Content 2 dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
     {
       title: "section 3",
       content:
-        "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+        "Content 3 dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
     },
   ];
   return (
     <div>
-      <menu-accordion
-        content-list={JSON.stringify(contentList)}
-      ></menu-accordion>
+      <div style={{ marginBottom: "1rem" }}>
+        <menu-tabs tab-list={JSON.stringify(arrayData)}></menu-tabs>
+      </div>
+      <menu-accordion content-list={JSON.stringify(arrayData)}></menu-accordion>
     </div>
   );
 };
