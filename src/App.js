e43cfee0c4bb0cadbbe84200c0045f2fc8e8fa6c
cnerylozada <cnery@dynamicdevs.io>
2021-06-04 22:22:27 -0500

    feat: dux pattern

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
 import { useEffect } from "react";
-import store from "./redux/custom-store";
-import { addBug } from "./redux/reducer";
 
 const App = () => {
-  useEffect(() => {
-    store.subscribe(() => console.log("state change!"));
-    store.dispatch(addBug("first bug"));
-    console.log(store.getState());
-  });
+  useEffect(() => {});
   return <div className="App"></div>;
 };
 
