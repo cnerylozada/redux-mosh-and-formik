a69e44a4ce29e1ed0b77b6e1b70cffefb26764cc
cnerylozada <cnery@dynamicdevs.io>
2021-07-25 16:12:12 -0500

    feat: creating reducers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import { useEffect } from "react";
-import { addBug, resolveBugById } from "./store/bugs";
+import { addBug, removeBugById, resolveBugById } from "./store/bugs";
 import { store } from "./store/configure-store";
 
 const App = () => {
+  store.subscribe(() => console.log(store.getState()));
+
   store.dispatch(addBug({ description: "my first bug" }));
   store.dispatch(addBug({ description: "my second bug" }));
   store.dispatch(addBug({ description: "my third bug" }));
   store.dispatch(resolveBugById({ id: 2 }));
+  store.dispatch(removeBugById({ id: 1 }));
 
   return <div className="App"></div>;
 };
--- src/store/bugs.js
+++ src/store/bugs.js
@@ -1,30 +1,25 @@
-import { createAction } from "@reduxjs/toolkit";
+import { createAction, createReducer } from "@reduxjs/toolkit";
 
 export const addBug = createAction("BUG_ADDED");
 export const removeBugById = createAction("BUG_REMOVED");
 export const resolveBugById = createAction("BUG_RESOLVED");
 
-const initialState = [];
 let idCounter = 0;
 
-export const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case addBug.type:
-      return [
-        ...state,
-        {
-          id: ++idCounter,
-          description: action.payload.description,
-          resolved: false,
-        },
-      ];
-    case removeBugById.type:
-      return state.filter((_) => _.id !== action.payload.id);
-    case resolveBugById.type:
-      return state.map((_) => {
-        return _.id === action.payload.id ? { ..._, resolved: true } : _;
-      });
-    default:
-      return state;
-  }
-};
+export const reducer = createReducer([], {
+  [addBug.type]: (bugs, action) => {
+    bugs.push({
+      id: ++idCounter,
+      description: action.payload.description,
+      resolved: false,
+    });
+  },
+  [removeBugById.type]: (bugs, action) => {
+    const index = bugs.findIndex((_) => _.id === action.payload.id);
+    bugs.splice(index, 1);
+  },
+  [resolveBugById.type]: (bugs, action) => {
+    const index = bugs.findIndex((_) => _.id === action.payload.id);
+    bugs[index].resolved = true;
+  },
+});
